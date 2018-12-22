e669958bd8e6724eac62ae7e1ef81cc84e29289f
Laurence Martinez <laurence.martinez14@gmail.com>
2018-12-22 03:04:25 -0600

    Updates

---
+++ precache-manifest.ca5bf11d94e847cdf3b441c6a3ff8aa2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5cb60181807771dc45e",
+    "url": "/click-all-the-dogs/static/js/runtime~main.b5cb6018.js"
+  },
+  {
+    "revision": "389d89eb8d501357c26e",
+    "url": "/click-all-the-dogs/static/js/main.389d89eb.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/click-all-the-dogs/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "389d89eb8d501357c26e",
+    "url": "/click-all-the-dogs/static/css/main.b9c09819.chunk.css"
+  },
+  {
+    "revision": "9eb5a933b3eb454c0831493a0fa4da57",
+    "url": "/click-all-the-dogs/index.html"
+  }
+];
